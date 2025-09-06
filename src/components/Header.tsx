5b49e17cf8d981b66a977e031e3ef71404a21414
paulo borges de almeida <borgespaulo72@yahoo.com.br>
2025-09-06 16:47:13 -0300

    feat: initial routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,34 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="flex justify-around lg:justify-between items-center  bg-gray-800 text-white gap-5 w-[1280px] lg:w-[1000px] h-[100px] ">
       <div>
-        <a href="https://github.com/Paulo-Borges" target="_blank">
-          Dev<span className="text-amber-300">Borges</span>
-        </a>
+        <Link to="https://github.com/Paulo-Borges" target="_blank">
+        Dev<span className="text-amber-300">Borges</span>
+        </Link>
       </div>
       <nav className="flex gap-10">
         <a
-          href="/"
+          href="#inicio"
           className="border-b hover:border-amber-300/80 border-transparent transition-all duration-300"
         >
           Inicio
         </a>
         <a
-          href="/especilidades"
+          href="#especialidades"
           className="border-b hover:border-amber-300/80 border-transparent transition-all duration-300"
         >
           Especialidades
         </a>
         <a
-          href="/sobre"
+          href="#sobre"
           className="border-b hover:border-amber-300/80 border-transparent transition-all duration-300"
         >
           Sobre
         </a>
         <a
-          href="/projetos"
+          href="#projetos"
           className="border-b hover:border-amber-300/80 border-transparent transition-all duration-300"
         >
           Projetos
@@ -34,7 +36,7 @@ const Header = () => {
       </nav>
       <div className="">
         <button className="bg-amber-300/60  rounded-3xl hover:bg-amber-300 flex w-[100px] h-[40px] justify-center items-center">
-          <a href="/contato" className="text-sm font-semibold">
+          <a href="#contato" className="text-sm font-semibold">
             Contato
           </a>
         </button>
