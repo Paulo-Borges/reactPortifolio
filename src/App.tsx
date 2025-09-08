d249259e331113ec3abd9efe0cfefce91915eff6
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-09-08 07:31:32 -0300

    Starting component Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -169,6 +169,21 @@ function App() {
           </p>
         </div>
       </section>
+      <section
+        id="projetos"
+        className="flex flex-col items-center pt-28 w-[1280px] lg:w-[1000px] h-[500px] gap-9"
+      >
+        <h1 className="flex justify-center text-4xl font-bold my-10 text-amber-300/60 pl-30">
+          MEU PORTFÓLIO.
+        </h1>
+        <div className="flex w-[1200] gap-4">
+          <div className="bg-custom-image w-[300px] h-[400px] hover:bg-position[100% 100%]">
+            fylo
+          </div>
+          <div className="bg-custom-image1 w-[300px] h-[400px]">extension</div>
+          <div className="bg-custom-image2 w-[300px] h-[400px]">inwood</div>
+        </div>
+      </section>
     </>
   );
 }
