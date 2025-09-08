a2c942199c3cb5d5d0b6edc604bcd8f0d1d56783
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-09-08 06:33:04 -0300

    styles Sobre components

--- src/App.tsx
+++ src/App.tsx
@@ -106,29 +106,48 @@ function App() {
         id="sobre"
         className="flex justify-around items-center pt-28 w-[1280px] lg:w-[1000px] h-[500px] gap-9 "
       >
-        <div className="flex flex-col gap-5">
-          <img src={eu} alt="desenvolvedor" />
+        <div className="flex flex-col gap-5 w-screen">
+          <div className="">
+            <img
+              src={eu}
+              alt="desenvolvedor"
+              className="rounded-full border-4 border-amber-300/10 hover:border-amber-300/50"
+            />
+          </div>
           <div className="flex justify-center gap-7">
-            <Link to="https://github.com/Paulo-Borges" target="_blank">
-              <FaGithub />
+            <Link
+              to="https://github.com/Paulo-Borges"
+              target="_blank"
+              className="rounded-full border-2 border-amber-300/10 hover:border-amber-300/50 text-amber-300/60"
+            >
+              <FaGithub className="" />
             </Link>
 
             <Link
               to="https://www.linkedin.com/in/paulo-borges-de-almeida-b543b3242/"
               target="_blank"
+              className="rounded-full border-2 border-amber-300/10 hover:border-amber-300/50 text-amber-300/60"
             >
               <FaLinkedin />
             </Link>
 
-            <Link to="https://www.instagram.com/devborges8/" target="_blank">
+            <Link
+              to="https://www.instagram.com/devborges8/"
+              target="_blank"
+              className="rounded-full border-2 border-amber-300/10 hover:border-amber-300/50 text-amber-300/60"
+            >
               <FaInstagram />
             </Link>
           </div>
         </div>
-        <div>
-          <h2>muito prazer</h2>
-          <h3>sou Desenvolvedor Borges</h3>
-          <p>
+        <div className="flex flex-col gap-2">
+          <h2 className="flex uppercase text-2xl font-bold my-10 text-white ">
+            muito prazer,
+          </h2>
+          <h3 className="flex uppercase text-2xl font-bold my-10 text-amber-300/60">
+            sou Desenvolvedor Borges.
+          </h3>
+          <p className="text-white ">
             Minha jornada como desenvolvedor é guiada pela paixão em transformar
             ideias em realidade digital. Com uma base sólida em HTML, CSS e
             JavaScript, eu me especializei na criação de experiências de usuário
@@ -139,7 +158,7 @@ function App() {
             funcional, mas também mais seguro e fácil de manter em projetos de
             grande escala.
           </p>
-          <p>
+          <p className="text-white ">
             Além disso, tenho domínio em Tailwind CSS, o que me permite criar
             interfaces modernas e personalizadas com agilidade, mantendo um
             design consistente e de alta qualidade. O que me motiva é a busca
